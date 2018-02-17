42a6d81b89bfa994efd73f390b8045d885717eb7
kafkaphoenix <jaguilerapuerta@gmail.com>
2018-02-17 16:35:34 +0100

    added basic example

--- PizzaHashCode/src/prPizza/Main.java
+++ PizzaHashCode/src/prPizza/Main.java
@@ -1,5 +1,8 @@
 package prPizza;
 
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -10,6 +13,22 @@ public class Main {
 		
 		System.out.println(p.toString());
 		
+		List<Integer> l = p.fgen();
+		
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(l.toString());
+		
+		while(st.hasMoreTokens())
+		{
+			for(int i = 0; i< 4; i++)
+			{
+				sb.append(st.nextToken());
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb.toString());
+
 	}
 
 }
--- PizzaHashCode/src/prPizza/Pizza.java
+++ PizzaHashCode/src/prPizza/Pizza.java
@@ -2,6 +2,8 @@ package prPizza;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Pizza {
@@ -17,7 +19,7 @@ public class Pizza {
 	public void makePizza()
 	{
 		try {
-			Scanner sc = new Scanner(new File("small.in"));
+			Scanner sc = new Scanner(new File("example.in"));
 			
 			rows = sc.nextInt();
 			cols = sc.nextInt();
@@ -53,6 +55,7 @@ public class Pizza {
 					}
 				}
 			}
+			
 			sc.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -60,6 +63,121 @@ public class Pizza {
 		}
 		
 		
+	}
+	
+	private float fobj (int ocupadas)
+	{
+		return ocupadas / (float) rows * cols;
+	}
+	
+	public List<Integer> fgen ()
+	{
+		List <Integer> l = new ArrayList<>();
+		
+		int sr = 0, sc = 0, er = rows, ec = 1;
+
+		while(sc < rows)
+		{
+			while(sc < cols)
+			{
+				if (comprobar(sr, sc, er, ec))
+				{
+					l.add(sr);
+					l.add(sc);
+					l.add(er - 1);
+					l.add(ec - 1);
+					sc = (ec + 1) < cols ? (ec) : cols; 
+				}
+				else
+				{
+					if(((er - sr) + (ec - sc)) > max)
+					{
+						break;
+					}
+					else
+					{
+						ec++;
+					}
+				}
+			}
+			
+			er--;
+			
+			if (comprobar(sr, sc, er, ec))
+			{
+				l.add(sr);
+				l.add(sc);
+				l.add(er - 1);
+				l.add(ec - 1);
+				sr = (er + 1) < rows ? (er) : 0;
+			}
+				
+		}
+		
+		return l;
+	}
+	
+	private boolean comprobar(int sr, int sc, int er, int ec)
+	{
+		boolean ok = false;
+		int lt = 0, lm = 0;
+		char minActual = minActual();
+		int maxSlices = maxSlices();
+		int minIngr;
+		int minLocal;
+
+		
+		for(int i = sc; i < ec; i++)
+		{
+			for(int j = sr; j < er; j++)
+			{
+				if(pizza[j][i] == 'T')
+				{
+					lt++;
+				}
+				else
+				{
+					lm++;
+				}
+			}
+		}
+		
+		if(lt >= min && lm >= min && (lt + lm) <= max)
+		{
+			if(minActual == 'T')
+			{
+				minIngr = tom;
+				minLocal = lt;
+			}
+			else
+			{
+				minIngr = mush;
+				minLocal = lm;
+			}
+			
+			if(( minIngr / maxSlices) == 1)
+			{
+				if(minLocal == 1)
+				{
+					ok = true;
+				}
+				else
+				{
+					ok = false;
+				}
+			}
+			else
+			{
+				ok = true;
+			}
+		}		
+		
+		return ok;
+	}
+	
+	private char minActual ()
+	{
+		return (tom < mush) ? 'T' : 'M';
 	}
 	
 	private int maxSlices ()
